3fb346d9c285bd41a40485e8c6cfc454a708a294
ninalealdesousa <46262177+ninalealdesousa@users.noreply.github.com>
2019-02-06 15:10:08 +0100

    oui

--- truc.js
+++ truc.js
@@ -1,3 +1,8 @@
 var button = document.getElementById('button');
 
 button.addEventListener("click", function();
+
+
+	$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
